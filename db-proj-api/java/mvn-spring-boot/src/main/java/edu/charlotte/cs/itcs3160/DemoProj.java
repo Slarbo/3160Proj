e6dbdec43ad1d7677ccf1cbd3329c5d04250faeb
Slarbo <145502543+Slarbo@users.noreply.github.com>
2024-04-16 16:35:38 -0400

    Ye
    Sure.

--- db-proj-api/java/mvn-spring-boot/src/main/java/edu/charlotte/cs/itcs3160/DemoProj.java
+++ db-proj-api/java/mvn-spring-boot/src/main/java/edu/charlotte/cs/itcs3160/DemoProj.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,7 +54,54 @@ public class DemoProj {
         }
     }
 
+    private boolean validateToken(String token) {
+        Boolean validated = false;
 
+        Connection conn = RestServiceApplication.getConnection();
+
+        try {
+            Statement stmt = conn.createStatement();
+            int affectedRows = stmt.executeUpdate("delete from tokens where timeout<current_timestamp");
+            conn.commit();
+
+            PreparedStatement ps = conn.prepareStatement("select username from tokens where token = ?");
+            ps.setString(1, token);
+            ResultSet rows = ps.executeQuery();
+
+            if (!rows.next())
+                validated=false;                 
+            else
+                validated=true;
+        } 
+        catch (SQLException ex) {
+            logger.error("Error in DB", ex);
+            validated=false;
+        }
+        finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                logger.error("Error in DB", ex);
+                validated=false;
+            }
+        }
+
+        return validated;
+    }
+
+    private Map<String, Object> invalidToken() {
+        Map<String, Object> returnData = new HashMap<String, Object>();
+
+        returnData.put("status", 400);
+        returnData.put("messAge", "a valid token is missing");
+
+        return returnData;
+    }
+
+    // To use JWT tokens
+    private JwtUtil jwtUtil = new JwtUtil();
+
+    // For logging
     private static final Logger logger = LoggerFactory.getLogger(DemoProj.class);
 
     @GetMapping("/")
@@ -66,40 +114,153 @@ public class DemoProj {
                 + "<br/>";
     }
 
-    /**
-     * Demo GET
-     *
-     *
-     * Obtain all users, in JSON format
-     *
-     * To use it, access: <br>
-     * http://localhost:8080/users/
-     *
-     *
-     * @return
-     */
-    @GetMapping(value = "/auction/", produces = "application/json")
-    @ResponseBody
-    public Map<String, Object> getAllAuctions() {
-        logger.info("###              DEMO: GET /auction             ###");
+    // Login user using simple tokens
+    // curl -X PUT http://localhost:8080/login -H "Content-Type: application/json" -d '{"username": "ssmith", "password": "ssmith_pass"}'
+
+    @PutMapping("/login")
+    public Map<String, Object> loginUser(@RequestBody Map<String, Object> payload) {
+        Map<String, Object> returnData = new HashMap<String, Object>();
+        
+        if (!payload.containsKey("username") || !payload.containsKey("password"))
+        {
+            logger.warn("missing credentials");
+            returnData.put("status", StatusCode.API_ERROR.code());
+            returnData.put("errors", "missing credentials");
+            return returnData;    
+        }
+        
+        String username = (String) payload.get("username");
+        String password = (String) payload.get("password");
+
         Connection conn = RestServiceApplication.getConnection();
+
+        try (PreparedStatement stmt = conn.prepareStatement("select 1 from users where username = ? and password = ?")) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rows = stmt.executeQuery();
+
+            if (!rows.next()) {
+                logger.warn("invalid credentials");
+                returnData.put("status", StatusCode.API_ERROR.code());
+                returnData.put("errors", "invalid credentials");
+                return returnData;                 
+            }
+            else {
+                int rnd=(int) (Math.random()*999999999+111111111);
+                String token = username + rnd;
+
+                PreparedStatement stmt1 = conn.prepareStatement("insert into tokens values( ?, ? , current_timestamp + (60 * interval '1 min'))");
+                stmt1.setString(1, username);
+                stmt1.setString(2, token);
+                int affectedRows = stmt1.executeUpdate();
+
+                returnData.put("status", StatusCode.SUCCESS.code());
+                returnData.put("token", token);
+            }
+
+            conn.commit();
+        } 
+        catch (SQLException ex) {
+            logger.error("Error in DB", ex);
+            returnData.put("status", StatusCode.INTERNAL_ERROR.code());
+            returnData.put("errors", ex.getMessage());;
+        }
+        finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                logger.error("Error in DB", ex);
+            }
+        }
+
+        return returnData;
+    }
+
+    // Login user using JWT tokens
+    // curl -X PUT http://localhost:8080/loginJWT -H "Content-Type: application/json" -d '{"username": "ssmith", "password": "ssmith_pass"}'
+
+    @PutMapping("/loginJWT")
+    public Map<String, Object> loginUserJWT(@RequestBody Map<String, Object> payload) {
+        Map<String, Object> returnData = new HashMap<String, Object>();
+        
+        if (!payload.containsKey("username") || !payload.containsKey("password"))
+         {
+            logger.warn("missing credentials");
+            returnData.put("status", StatusCode.API_ERROR.code());
+            returnData.put("errors", "missing credentials");
+            return returnData;    
+        }
+        
+        String username = (String) payload.get("username");
+        String password = (String) payload.get("password");
+
+        Connection conn = RestServiceApplication.getConnection();
+
+        try (PreparedStatement stmt = conn.prepareStatement("select 1 from users where username = ? and password = ?")) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rows = stmt.executeQuery();
+
+            if (!rows.next()) {
+                logger.warn("invalid credentials");
+                returnData.put("status", StatusCode.API_ERROR.code());
+                returnData.put("errors", "invalid credentials");
+                return returnData;                 
+            }
+            else {
+                String token = jwtUtil.generateTokenJWT(username);
+                returnData.put("status", StatusCode.SUCCESS.code());
+                returnData.put("token", token);
+            }
+
+        } 
+        catch (SQLException ex) {
+            logger.error("Error in DB", ex);
+            returnData.put("status", StatusCode.INTERNAL_ERROR.code());
+            returnData.put("errors", ex.getMessage());;
+        }
+        finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                logger.error("Error in DB", ex);
+            }
+        }
+
+        return returnData;
+    }
+
+    // List departments using simpe tokens
+    // To use JWT tokens just modify @token_required to @token_required_jwt
+    // curl -X GET http://localhost:8080/departments -H "Content-Type: application/json" -H "x-access-tokens: eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzc21pdGgiLCJleHAiOjE3MTMxMTYyMjh9.FFHTvCmiXWmAh5tKKMeC1HH4DLPAB9d9IqqFYO2U3JOMCKjMO1qHNUIMYmmP0nfXXw56UbTOsfZOrVeOnCwquA"
+    // curl -X GET http://localhost:8080/departments -H "Content-Type: application/json" -H "x-access-tokens: ssmith339965530"
+
+    @GetMapping(value = "/departments", produces = "application/json")
+    @ResponseBody
+    public Map<String, Object> getAllDepartments(@RequestHeader("x-access-tokens") String token) {
+        // Token validation if using JWT
+        //if (!jwtUtil.validateTokenJWT(token))
+        //    return invalidToken();
+        // Simple token validation 
+        if (!validateToken(token))
+            return invalidToken();
+
+        logger.info("###              DEMO: GET /departments              ### ");
+        
         Map<String, Object> returnData = new HashMap<String, Object>();
         List<Map<String, Object>> results = new ArrayList<>();
 
+        Connection conn = RestServiceApplication.getConnection();
+
         try (Statement stmt = conn.createStatement()) {
-            ResultSet rows = stmt.executeQuery("SELECT aid, isbn, start_date, end_date, current_bid, description FROM auction");
-            logger.debug("---- auctions  ----");
+            ResultSet rows = stmt.executeQuery("SELECT deptno, dname, loc FROM dept");
+            logger.debug("---- departments  ----");
             while (rows.next()) {
                 Map<String, Object> content = new HashMap<>();
-                logger.debug("'aid': {}, 'isbn': {}, 'start_date': {}, 'end_date': {}, 'current_bid': {}, 'description': {}",
-                        rows.getString("aid"), rows.getString("isbn"), rows.getString("start_date"), rows.getString("end_date"), rows.getString("current_bid"), rows.getString("description")
-                );
-                content.put("aid", rows.getString("aid"));
-                content.put("ISBN", rows.getString("isbn"));
-                content.put("start_date", rows.getString("start_date"));
-                content.put("end_date", rows.getString("end_date"));
-                content.put("current_bid", rows.getString("current_bid"));
-                content.put("description", rows.getString("description"));
+
+                content.put("deptno", rows.getString("deptno"));
+                content.put("dname", rows.getString("dname"));
+                content.put("loc", rows.getString("loc"));
                 results.add(content);
             }
 
@@ -111,171 +272,89 @@ public class DemoProj {
             returnData.put("status", StatusCode.INTERNAL_ERROR.code());
             returnData.put("errors", ex.getMessage());
         }
-        return returnData;
-    }
-
-    /**
-     * Demo GET
-     *
-     *
-     * Obtain user with {@code username}
-     *
-     * To use it, access: <br>
-     * http://localhost:8080/users/ssmith
-     *
-     *
-     * @param username id of the user to be selected
-     * @return data of the user
-     */
-    @GetMapping(value = "/users/{username}", produces = "application/json")
-    @ResponseBody
-    public Map<String, Object> getuser(
-            @PathVariable("username") String username
-    ) {
-        logger.info("###              DEMO: GET /users              ###");
-        Connection conn = RestServiceApplication.getConnection();
-
-        Map<String, Object> returnData = new HashMap<String, Object>();
-
-        Map<String, Object> content = new HashMap<>();
-        try (PreparedStatement ps = conn.prepareStatement("SELECT username, name, city FROM users WHERE username = ?")) {
-            ps.setString(1, username);
-            ResultSet rows = ps.executeQuery();
-            logger.debug("---- selected user  ----");
-            if (rows.next()) {
-                logger.debug("'username': {}, 'name': {}, 'city': {}", rows.getString("username"), rows.getString("name"), rows.getString("city"));
-                content.put("username", rows.getString("username"));
-                content.put("name", rows.getString("name"));
-                content.put("city", rows.getString("city"));
-            }
-
-            returnData.put("status", StatusCode.SUCCESS.code());
-            returnData.put("results", content);
-
-        } catch (SQLException ex) {
-            logger.error("Error in DB", ex);
-            returnData.put("status", StatusCode.INTERNAL_ERROR.code());
-            returnData.put("errors", ex.getMessage());
-        }
-        return returnData;
-    }
-
-    /**
-     * Demo POST
-     *
-     *
-     * Add a new user in a JSON payload
-     *
-     * To use it, you need to use postman or curl:
-     *
-     * {@code curl -X POST http://localhost:8080/users/ -H "Content-Type: application/json" -d
-     * '{"city": "London", "username": "ppopov", "name": "Peter Popov"}'
-     *
-     *
-     */
-    @PostMapping(value = "/users/", consumes = "application/json")
-    @ResponseBody
-    public Map<String, Object> createuser(
-            @RequestBody Map<String, Object> payload
-    ) {
-
-        logger.info("###              DEMO: POST /users              ###");
-        Connection conn = RestServiceApplication.getConnection();
-
-        logger.debug("---- new user  ----");
-        logger.debug("payload: {}", payload);
-
-        Map<String, Object> returnData = new HashMap<String, Object>();
-
-        // validate all the required inputs and types, e.g.,
-        if (!payload.containsKey("city")) {
-            logger.warn("city are required to update");
-            returnData.put("status", StatusCode.API_ERROR.code());
-            returnData.put("errors", "city are required to update");
-            return returnData;
-        }
-
-        try (PreparedStatement ps = conn.prepareStatement(""
-                + "INSERT INTO users (username, name, city) "
-                + "         VALUES (  ? ,   ? ,    ? )")) {
-            ps.setString(1, (String) payload.get("username"));
-            ps.setString(2, (String) payload.get("name"));
-            ps.setString(3, (String) payload.get("city"));
-            int affectedRows = ps.executeUpdate();
-            conn.commit();
-
-            returnData.put("status", StatusCode.SUCCESS.code());
-            returnData.put("results", "user inserted successfully");
-
-        } catch (SQLException ex) {
-            logger.error("Error in DB", ex);
-            try {
-                conn.rollback();
-            } catch (SQLException ex1) {
-                logger.warn("Couldn't rollback", ex);
-            }
-
-            returnData.put("status", StatusCode.INTERNAL_ERROR.code());
-            returnData.put("errors", ex.getMessage());
-
-        } finally {
+        finally {
             try {
                 conn.close();
             } catch (SQLException ex) {
                 logger.error("Error in DB", ex);
             }
         }
+
         return returnData;
     }
 
-    /**
-     * Demo PUT
-     *
-     *
-     * Update a user based on the a JSON payload
-     *
-     * o use it, you need to use postman or curl:
-     *
-     * {@code curl -X PUT http://localhost:8080/users/ -H "Content-Type: application/json" -d '{"city": "Raleigh"}'
-     *
-     */
-    @PutMapping(value = "/users/{username}", consumes = "application/json")
+    // Add empolyee
+    // curl -X POST http://localhost:8080/emp/ -H 'Content-Type: application/json' -H "x-access-tokens: ssmith339965530" -d '{"ename": "PETER", "job": "ANALYST", "sal": 100, "dname": "SALES"}'
+
+    @PostMapping(value = "/emp/", consumes = "application/json")
     @ResponseBody
-    public Map<String, Object> updateuser(
-            @PathVariable("username") String username,
+    public Map<String, Object> addEmployee(
+            @RequestHeader("x-access-tokens") String token,
             @RequestBody Map<String, Object> payload
     ) {
+        // Token validation if using JWT
+        //if (!jwtUtil.validateTokenJWT(token))
+        //    return invalidToken();
+        // Simple token validation 
+        if (!validateToken(token))
+            return invalidToken();
 
-        logger.info("###              DEMO: PUT /users               ###");
+        logger.info("###              DEMO: POST /Add Employee           ###");
+        Connection conn = RestServiceApplication.getConnection();
+
+        logger.debug("---- new employee  ----");
+        logger.debug("payload: {}", payload);
 
         Map<String, Object> returnData = new HashMap<String, Object>();
 
         // validate all the required inputs and types, e.g.,
-        if (!payload.containsKey("city")) {
-            logger.warn("city are required to update");
+        if ((!payload.containsKey("ename")) || (!payload.containsKey("job")) || (!payload.containsKey("sal"))  || (!payload.containsKey("dname"))) {
+            logger.warn("missing inputs");
             returnData.put("status", StatusCode.API_ERROR.code());
-            returnData.put("errors", "city are required to update");
+            returnData.put("errors", "missing inputs");
             return returnData;
         }
 
-        logger.info("---- update user  ----");
-        logger.debug("content: {}", payload);
-        Connection conn = RestServiceApplication.getConnection();
+        try {
+            PreparedStatement ps = conn.prepareStatement("select deptno from dept where dname = ?");
+            ps.setString(1, (String) payload.get("dname"));
+            ResultSet rows = ps.executeQuery();
 
-        try (PreparedStatement ps = conn.prepareStatement(""
-                + "UPDATE users"
-                + "   SET city = ? "
-                + " WHERE username = ?")) {
+            if (rows.next()) {
+                int deptno = rows.getInt("deptno");
 
-            ps.setString(1, (String) payload.get("city"));
-            ps.setString(2, username);
+                // get new empno - may generate duplicate keys, wich will lead to an exception
+                Statement stmt = conn.createStatement();
+                rows = stmt.executeQuery("select coalesce(max(empno),1) empno from emp");
+                rows.next();
+                int empno = rows.getInt("empno")+1;
 
-            int affectedRows = ps.executeUpdate();
-            conn.commit();
+                ps  = conn.prepareStatement("INSERT INTO emp (empno,ename,job,sal,deptno) VALUES (?, ?, ?, ?, ?)");
+                ps.setInt(1, empno);
+                ps.setString(2, (String) payload.get("ename"));
+                ps.setString(3, (String) payload.get("job"));
+                ps.setInt(4, (int) payload.get("sal"));
+                ps.setInt(5, deptno);
+                int affectedRows = ps.executeUpdate();
 
-            returnData.put("status", StatusCode.SUCCESS.code());
-            returnData.put("results", "user updated successfully");
+                if (affectedRows==1) {
+                    returnData.put("status", StatusCode.SUCCESS.code());
+                    returnData.put("empno", empno);
 
+                    conn.commit();
+                }
+                else {
+                    returnData.put("status", StatusCode.API_ERROR.code());
+                    returnData.put("errors", "Could not insert");
+
+                    conn.rollback();
+                }                
+            }
+            else {
+                returnData.put("status", StatusCode.API_ERROR.code());
+                returnData.put("results", "department does not exist");
+                conn.rollback();    
+            }
         } catch (SQLException ex) {
             logger.error("Error in DB", ex);
             try {
@@ -286,6 +365,7 @@ public class DemoProj {
 
             returnData.put("status", StatusCode.INTERNAL_ERROR.code());
             returnData.put("errors", ex.getMessage());
+
         } finally {
             try {
                 conn.close();
